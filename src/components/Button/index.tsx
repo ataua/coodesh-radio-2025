a76e15cb6cb331e35ccc036b79aaea441043c566
Atauã Pinali Doederlein <ataua.doederlein@gmail.com>
2025-03-11 22:22:18 -0300

    update Button
    add intent

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,28 @@
-const Button = ({ onClick, text }: { onClick: () => void, text: string | number | null }) => {
+const intentOptions: { [key: string]: string } = {
+    primary: "bg-blue-500 hover:bg-blue-700",
+    secondary: "bg-gray-500 hover:bg-gray-700",
+    success: "bg-green-500 hover:bg-green-700",
+    danger: "bg-red-500 hover:bg-red-700",
+    warning: "bg-yellow-500 hover:bg-yellow-700",
+}
+
+interface IButtonProps {
+    onClick: () => void;
+    text: string | number | null;
+    intent?: typeof intentOptions[keyof typeof intentOptions]; // that allows extension of the object 
+}
+
+const Button = ({ onClick, text, intent = 'primary' }: IButtonProps) => {
+
+    const buttonClass = `${intentOptions[intent]} hover:cursor-pointer text-white font-bold py-2 px-4 rounded `
+
     return (
         <button
             onClick={onClick}
-            className="bg-blue-500 hover:bg-blue-700 hover:cursor-pointer text-white font-bold py-2 px-4 rounded"
-        >{text}</button>
+            className={buttonClass}
+        >
+            {text}
+        </button >
     )
 }
 
